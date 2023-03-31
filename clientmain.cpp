cbefcbce1d11b7eeb66e7735abfa3c7a9b4a6ab0
node189 <node189@icloud.com>
2023-03-31 12:57:29 +0800

    feat: update client

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,11 +1,17 @@
 #include <arpa/inet.h>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <sys/socket.h>
+#include <unistd.h>
 
 /* You will to add includes here */
 
@@ -15,6 +21,9 @@
 
 // Included to get the support library
 #include "calcLib.h"
+
+const int gVersionCount = 3;
+const char gVersions[3][10] = {"1.0", "1.1", "1.2"};
 
 /**
  * @brief Translate host to IP
@@ -73,11 +82,86 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  // 连接服务器
+  // Connenct to the server
   if (connect(sock, (struct sockaddr *)serv_addr, sizeof(sockaddr_in)) < 0) {
     printf("Connection Failed\n");
     return -1;
   }
+
+  const int N = 1024;
+  char buffer[N] = {0};
+  int len = 0;
+  bool is_header = true;
+
+  // a stack to store package
+  char package[N];
+  int tail = 0;
+
+  // The message which needed to be sent when the version is legal
+  char version_ok_message[] = "OK\n";
+  int version_ok_message_len = strlen(version_ok_message);
+
+  char cur_version[10];
+  memset(cur_version, 0, sizeof(cur_version));
+  do {
+    memset(buffer, 0, N); // Clear buffer
+    len = read(sock, buffer, N);
+
+    // Handle server message package
+    for (int i = 0; i < len; i++) {
+      if (buffer[i] != '\n') {
+        package[tail++] = buffer[i];
+      } else {
+
+#ifdef DEBUG
+        printf("Receive server message: [%s]\n", package);
+#endif
+
+        if (tail == 0) {
+          // package header finished
+          is_header = false;
+
+#ifdef DEBUG
+        printf("Current protocol version is: [%s]\n", cur_version);
+#endif
+          // check version
+          bool flag = false;
+          int cur_version_len = strlen(cur_version);
+          int target_version_len;
+          for (int i = 0; i < gVersionCount; i++) {
+            target_version_len = strlen(gVersions[i]);
+
+            if (target_version_len != cur_version_len) {
+              continue;
+            } else if (strncmp(gVersions[i], cur_version, cur_version_len)) {
+              flag = true;
+            }
+          }
+
+          if (flag) {
+            write(sock, version_ok_message, version_ok_message_len);
+          } else {
+            puts("Protocol version error");
+            return -1;
+          }
+        } else if (is_header) {
+          // handle package header
+          char *cur_package = package;
+          char sep[] = " ";
+
+          strtok(cur_package, sep);
+          strtok(NULL, sep);
+          char *version = strtok(NULL, sep);
+          strncpy(cur_version, version, strlen(version) - 1);
+        } else {
+          // handle package body
+        }
+
+        tail = 0;
+        memset(package, 0x0, sizeof package);
+      }
+    }
+  } while (len >= 0);
 
   free(serv_addr);
 }
